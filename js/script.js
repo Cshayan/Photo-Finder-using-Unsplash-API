2ded889bf48cebf067977894a9f1075181f62701
Shayan Chatterjee <shayanchatterjee7@gmail.com>
2019-06-11 20:03:54 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+const accessKey = 'de7cf08644eec967f2450834c52fbc8c19ac8bd7c209d542b825e9662d939ba6';
+var page = 1;
+var searchInput;
+
+const form = document.getElementById('form');
+const errorMsg = document.getElementById('errorMsg');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // document.getElementById('results').empty();
+    $('#results').empty();
+    searchInput = document.getElementById('searchInput').value;
+    if (searchInput !== '') {
+        init(searchInput);
+    } else {
+        errorMsg.innerHTML = 'Please enter something to find the images.'
+        document.getElementById('error').classList.remove('hide');
+    }
+});
+
+function init(searchInput) {
+
+    var q = "https://api.unsplash.com/search/collections?&client_id=" + accessKey + "&page=" + page + "&query=" + searchInput;
+    fetch(q)
+        .then(result => {
+            return result.json()
+        })
+        .then(result => {
+            getData(result)
+        });
+}
+
+function getData(data) {
+    console.log(data);
+
+    var length = data.results.length;
+    if (length == 0) {
+        errorMsg.innerHTML = 'No results found';
+        document.getElementById('error').classList.remove('hide');
+    } else {
+        document.getElementById('error').classList.add('hide');
+        for (var i = 0; i < length; i++) {
+            var imageSrc = data.results[i].cover_photo.urls.small;
+            var downloadURL = data.results[i].cover_photo.links.download;
+            $('#results').append(`
+                <div class="col-md-4">
+                <div class="card card-body text-center mt-3">
+                     <img class="img-thumbnail movieImg" id="img" alt="No picture available" src="${imageSrc}">
+                 </div>
+                <div>
+                `);
+
+            if (downloadURL !== '') {
+                document.getElementById('img').click(function () {
+                    var win = window.open(downloadURL, '_blank');
+                    if (!win) {
+                        alert('Please allow popups for this website');
+                    }
+                });
+            }
+        }
+        page = page + 1;
+        $(window).scroll(function () {
+            if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+                init(searchInput);
+            }
+        });
+    }
+
+}
